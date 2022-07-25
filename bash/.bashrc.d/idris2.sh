f0729812bffa811bf6ad582b531e796cb7dc9270
Bertalan Kis <kbertalan@gmail.com>
2022-07-25 16:45:38 +0200

    add idris2 pack support

--- bash/.bashrc.d/idris2.sh
+++ bash/.bashrc.d/idris2.sh
@@ -1,5 +1,10 @@
-if [ -d $HOME/.idris2/bin ]; then
+if [ -d $HOME/.pack/bin ]; then
+  PATH=$HOME/.pack/bin:$PATH
+  eval "$(pack completion-script pack)"
+elif [ -d $HOME/.idris2/bin ]; then
   PATH=$HOME/.idris2/bin:$PATH
-  eval "$(idris2 --bash-completion-script idris2)"
 fi
 
+if has idris2; then
+  eval "$(idris2 --bash-completion-script idris2)"
+fi
